4b899116ec8248ec0d2216f26e5c6d5bad7bdbcb
Jonathan Zhao <jonathanzhaon@gmail.com>
2021-10-23 23:43:52 -0400

    Added Basic Text to sliders

--- lib/input_page/input_page.dart
+++ lib/input_page/input_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'slider_widget.dart';
 
 class InputPage extends StatelessWidget {
-  List<double> inputData = [0, 1, 2, 3, 4, 5]; //List of values to store inputs
+  List<double> inputData = [0, 0, 0, 0, 0, 0]; //List of values to store inputs
   InputPage({Key? key}) : super(key: key);
 
   @override
@@ -10,38 +10,43 @@ class InputPage extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       body: Column(children: [
+        Text('Sleep(In Hours)'),
         SliderStateful(
           min: 0.0,
           max: 10.0,
           divisions: 10,
-          currentSliderValue: 1.0,
+          currentSliderValue: 0,
           onChangedCallback: (value) {
             inputData[0] = value;
           },
         ),
+        Text('# of Bathroom Trips'),
+        Text('Discomfort of Bathroom Trips'),
         SliderStateful(
           min: 0.0,
           max: 10.0,
           divisions: 10,
-          currentSliderValue: 1.0,
+          currentSliderValue: 0,
           onChangedCallback: (value) {
             inputData[1] = value;
           },
         ),
+        Text('Stress Level'),
         SliderStateful(
           min: 0.0,
           max: 10.0,
           divisions: 10,
-          currentSliderValue: 1.0,
+          currentSliderValue: 0,
           onChangedCallback: (value) {
             inputData[2] = value;
           },
         ),
+        Text('Exercise'),
         SliderStateful(
           min: 0.0,
           max: 10.0,
           divisions: 10,
-          currentSliderValue: 1.0,
+          currentSliderValue: 0,
           onChangedCallback: (value) {
             inputData[3] = value;
           },
