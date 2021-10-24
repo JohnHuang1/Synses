828c1e1642726a18e0fe07bc2c28a194681c138d
John Huang <46700107+JohnHuang1@users.noreply.github.com>
2021-10-24 02:27:07 -0400

    hive with graph implementation

--- lib/graphs_page/graphs_page.dart
+++ lib/graphs_page/graphs_page.dart
@@ -1,10 +1,12 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:synses/data/entry.dart';
+import 'package:synses/shared/boxes.dart';
 
 class GraphPage extends StatefulWidget {
   String title = '';
+
   GraphPage({Key? key, required this.title}) : super(key: key);
 
   State<GraphPage> createState() => GraphPageStat();
@@ -31,50 +33,68 @@ void initState(){
   }
 */
 
-  List<IBSData> _chartData = [];
 
   @override
   void initState() {
-    _chartData = getChartData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: SfCartesianChart(
-        title: ChartTitle(text: 'Factors & Severity'),
-
-        series: <ChartSeries>[
-          LineSeries<IBSData, double>(
-              dataSource: _chartData,
-              xValueMapper: (IBSData severity, _) => severity.factor,
-              yValueMapper: (IBSData severity, _) => severity.severity,
-              dataLabelSettings:
-                  DataLabelSettings(isVisible: true, color: Colors.blue))
-        ],
-        primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
-        // primaryYAxis: NumericAxis(
-        // numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
-      ),
-    ));
+    return ValueListenableBuilder<Box<Entry>>(
+        valueListenable: Boxes.getEntries().listenable(),
+        builder: (context, box, _) {
+          return buildContent(box);
+        });
   }
 
-  List<IBSData> getChartData() {
-    final List<IBSData> chartData = [
-      IBSData(2, 3),
-      IBSData(3, 6),
-      IBSData(4, 9),
-      IBSData(6, 5),
-      IBSData(7, 1)
-    ];
-    return chartData;
+  Widget buildContent(Box<Entry> box) {
+    return SafeArea(
+      child: Scaffold(
+        body: SfCartesianChart(
+          title: ChartTitle(text: 'Factors & Severity'),
+
+          series: <ChartSeries>[
+            LineSeries<IBSData, double>(
+                dataSource: getChartData(box),
+                xValueMapper: (IBSData severity, _) => severity.factor,
+                yValueMapper: (IBSData severity, _) => severity.severity,
+                dataLabelSettings:
+                    DataLabelSettings(isVisible: true, color: Colors.blue))
+          ],
+          primaryXAxis:
+              NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+          // primaryYAxis: NumericAxis(
+          // numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
+        ),
+      ),
+    );
+  }
+
+  List<IBSData> getChartData(Box<Entry> box) {
+    // final List<IBSData> chartData = [
+    //   IBSData(2, 3),
+    //   IBSData(3, 6),
+    //   IBSData(4, 9),
+    //   IBSData(6, 5),
+    //   IBSData(7, 1)
+    // ];
+    int index = 0;
+    List<IBSData> data = box.values
+        .where((element) => element.getEntryType() == 'Sleep')
+        .map((e) => IBSData((index++).toDouble(), e.timeSlept!))
+        .toList();
+
+    // print("length = ${data.length}");
+    // data.forEach((element) {print("data: ${element.factor} | ${element.severity}");});
+
+    return data;
   }
 }
 
 class IBSData {
   IBSData(this.factor, this.severity);
+
   final double factor;
   final double severity;
 }
