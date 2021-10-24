86bd7c06f48750b45563aa7ddb7c68917f50a19d
Eric Zhang <ez38@duke.edu>
2021-10-24 03:52:07 -0400

    finished input_page.dart

--- lib/input_page/input_page.dart
+++ lib/input_page/input_page.dart
@@ -18,7 +18,6 @@ class InputPage extends StatelessWidget {
           mainAxisSpacing: 10,
           crossAxisCount: 3,
           children: [
-<<<<<<< Updated upstream
             InputDialogStateful(
                 //Sleep
                 onInputCallback: (value) {
@@ -26,87 +25,42 @@ class InputPage extends StatelessWidget {
                 },
                 title: 'Total Hours Slept',
                 icon: Icon(Icons.airline_seat_individual_suite)),
-            InputDialogStateful(
+            textSliderStateful(
                 //Bathroom
                 onInputCallback: (value) {
-                  inputData[0] = value;
+                  inputData[1] = value;
                 },
                 title: 'Bathroom: Discomfort Level',
                 icon: Icon(Icons.airline_seat_legroom_reduced_sharp)),
             InputDialogStateful(
                 //Mood
                 onInputCallback: (value) {
-                  inputData[0] = value;
+                  inputData[2] = value[0];
+                  inputData[3] = value[1];
                 },
                 title: 'Mood/Stress Level',
                 icon: Icon(Icons.add_reaction)),
             InputDialogStateful(
                 //Exercise
                 onInputCallback: (value) {
-                  inputData[0] = value;
+                  inputData[4] = value;
                 },
                 title: 'Exercise Intensity',
                 icon: Icon(Icons.add_reaction)),
             InputDialogStateful(
                 //Diet
                 onInputCallback: (value) {
-                  inputData[0] = value;
+                  inputData[5] = value;
                 },
                 title: 'Diet',
                 icon: Icon(Icons.food_bank)),
             InputDialogStateful(
                 //Hydration
                 onInputCallback: (value) {
-                  inputData[0] = value;
+                  inputData[6] = value;
                 },
                 title: 'Hydration level',
                 icon: Icon(Icons.add_reaction)),
-=======
-            SliderDialog(
-                inputCallback: (value) {
-                  inputData[0] = value;
-                },
-                title: 'Total Hours Slept'),
-            Text('# of Bathroom Trips'),
-            Text('Discomfort of Bathroom Trips'),
-            SliderStateful(
-              min: 0.0,
-              max: 10.0,
-              divisions: 10,
-              currentSliderValue: 0,
-              onChangedCallback: (value) {
-                inputData[1] = value;
-              },
-            ),
-            Text('Stress Level'),
-            SliderStateful(
-              min: 0.0,
-              max: 10.0,
-              divisions: 10,
-              currentSliderValue: 0,
-              onChangedCallback: (value) {
-                inputData[2] = value;
-              },
-            ),
-            Text('Exercise'),
-            SliderStateful(
-              min: 0.0,
-              max: 10.0,
-              divisions: 10,
-              currentSliderValue: 0,
-              onChangedCallback: (value) {
-                inputData[3] = value;
-              },
-            ),
-            textSliderStateful(
-                onInputCallback: (value) {
-                  inputData[4] = value[0]; //trips to the bathroom
-                  inputData[5] = value[1]; //level of discomfort
-                  print(inputData[4]);
-                  print(inputData[5]);
-                },
-                title: 'Restroom Usage')
->>>>>>> Stashed changes
           ]),
     ));
   }
