9c97114de147030882ac4044fdf621a412ac257b
Jonathan Zhao <jonathanzhaon@gmail.com>
2021-10-24 02:53:05 -0400

    Gridview for input page

--- lib/input_page/input_page.dart
+++ lib/input_page/input_page.dart
@@ -10,44 +10,50 @@ class InputPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: Column(children: [
-        SliderDialog(
-            inputCallback: (value) {
-              inputData[0] = value;
-            },
-            title: 'Total Hours Slept'),
-        Text('# of Bathroom Trips'),
-        Text('Discomfort of Bathroom Trips'),
-        SliderStateful(
-          min: 0.0,
-          max: 10.0,
-          divisions: 10,
-          currentSliderValue: 0,
-          onChangedCallback: (value) {
-            inputData[1] = value;
-          },
-        ),
-        Text('Stress Level'),
-        SliderStateful(
-          min: 0.0,
-          max: 10.0,
-          divisions: 10,
-          currentSliderValue: 0,
-          onChangedCallback: (value) {
-            inputData[2] = value;
-          },
-        ),
-        Text('Exercise'),
-        SliderStateful(
-          min: 0.0,
-          max: 10.0,
-          divisions: 10,
-          currentSliderValue: 0,
-          onChangedCallback: (value) {
-            inputData[3] = value;
-          },
-        ),
-      ]),
+      body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          children: [
+            InputDialogStateful(
+                onInputCallback: (value) {
+                  inputData[0] = value;
+                },
+                title: 'Total Hours Slept'),
+            Text('# of Bathroom Trips'),
+            Text('Discomfort of Bathroom Trips'),
+            SliderStateful(
+              min: 0.0,
+              max: 10.0,
+              divisions: 10,
+              currentSliderValue: 0,
+              onChangedCallback: (value) {
+                inputData[1] = value;
+              },
+            ),
+            Text('Stress Level'),
+            SliderStateful(
+              min: 0.0,
+              max: 10.0,
+              divisions: 10,
+              currentSliderValue: 0,
+              onChangedCallback: (value) {
+                inputData[2] = value;
+              },
+            ),
+            Text('Exercise'),
+            SliderStateful(
+              min: 0.0,
+              max: 10.0,
+              divisions: 10,
+              currentSliderValue: 0,
+              onChangedCallback: (value) {
+                inputData[3] = value;
+              },
+            ),
+          ]),
     ));
   }
 }
