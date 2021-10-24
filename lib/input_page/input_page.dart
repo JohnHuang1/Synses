97981f5f79d7ca034481352399097c8bec6a3dbf
Jonathan Zhao <jonathanzhaon@gmail.com>
2021-10-23 23:14:43 -0400

    Slider Implementation
    Slider implementation completed, returns value. Created list to store final user values

--- lib/input_page/input_page.dart
+++ lib/input_page/input_page.dart
@@ -2,14 +2,51 @@ import 'package:flutter/material.dart';
 import 'slider_widget.dart';
 
 class InputPage extends StatelessWidget {
-  const InputPage({Key? key}) : super(key: key);
+  List<double> inputData = [0, 1, 2, 3, 4, 5]; //List of values to store inputs
+  InputPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SliderStateful(min: 1.0, max: 10.0, divisions: 10),
-      ),
-    );
+        home: Scaffold(
+      body: Column(children: [
+        SliderStateful(
+          min: 0.0,
+          max: 10.0,
+          divisions: 10,
+          currentSliderValue: 1.0,
+          onChangedCallback: (value) {
+            inputData[0] = value;
+          },
+        ),
+        SliderStateful(
+          min: 0.0,
+          max: 10.0,
+          divisions: 10,
+          currentSliderValue: 1.0,
+          onChangedCallback: (value) {
+            inputData[1] = value;
+          },
+        ),
+        SliderStateful(
+          min: 0.0,
+          max: 10.0,
+          divisions: 10,
+          currentSliderValue: 1.0,
+          onChangedCallback: (value) {
+            inputData[2] = value;
+          },
+        ),
+        SliderStateful(
+          min: 0.0,
+          max: 10.0,
+          divisions: 10,
+          currentSliderValue: 1.0,
+          onChangedCallback: (value) {
+            inputData[3] = value;
+          },
+        ),
+      ]),
+    ));
   }
 }
--- lib/input_page/slider_widget.dart
+++ lib/input_page/slider_widget.dart
@@ -1,36 +1,46 @@
 import 'package:flutter/material.dart';
 
 class SliderStateful extends StatefulWidget {
+  //Slider widget, can change values, returns final Slider Value
   double min = 0;
   double max = 0;
+  double currentSliderValue = 6;
   int divisions = 0;
+  Function onChangedCallback;
   SliderStateful(
-      {Key? key, required this.min, required this.max, required this.divisions})
+      {Key? key,
+      required this.min,
+      required this.max,
+      required this.divisions,
+      required this.currentSliderValue,
+      required this.onChangedCallback})
       : super(key: key);
 
   @override
-  State<SliderStateful> createState() =>
-      _SliderStatefulState(min, max, divisions);
+  State<SliderStateful> createState() => _SliderStatefulState(
+      min, max, divisions, currentSliderValue, onChangedCallback);
 }
 
-/// This is the private State class that goes with SliderStateful.
 class _SliderStatefulState extends State<SliderStateful> {
-  _SliderStatefulState(this.min, this.max, this.divisions);
-  double _currentSliderValue = 5;
+  _SliderStatefulState(this.min, this.max, this.divisions,
+      this.currentSliderValue, this.onChangedCallback);
+  double currentSliderValue;
   double min;
   double max;
   int divisions;
+  Function onChangedCallback;
   @override
   Widget build(BuildContext context) {
     return Slider(
-      value: _currentSliderValue,
+      value: currentSliderValue,
       min: min,
       max: max,
       divisions: divisions,
-      label: _currentSliderValue.round().toString(),
+      label: currentSliderValue.round().toString(),
       onChanged: (double value) {
         setState(() {
-          _currentSliderValue = value;
+          currentSliderValue = value;
+          onChangedCallback(value);
         });
       },
     );
